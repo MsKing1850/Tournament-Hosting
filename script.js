612078743e777bad924762dfb6059f7e25098e7e
MsKing1850 <iamg6102@gmail.com>
2025-02-27 14:01:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
  function register(game, map, mode, time, date, prize, entry) {
-        // Construct the URL with correctly encoded parameters
+        
         const url = `registration.html?game=${encodeURIComponent(game)}&map=${encodeURIComponent(map)}&mode=${encodeURIComponent(mode)}&time=${encodeURIComponent(time)}&date=${encodeURIComponent(date)}&prize=${encodeURIComponent(prize)}&entry=${encodeURIComponent(entry)}`;
         
-        console.log("Navigating to:", url); // Debugging log
+        console.log("Navigating to:", url);
         window.location.href = url;
     }
